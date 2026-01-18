b23978ef2ff75562ad21d5143fa0b1559f8cdab2
S0MS0MM1dA <durjoypodder76@gmail.com>
2026-01-18 12:23:37 +0600

    Added Applicants Page

---
+++ app/views/dashboard/recruiter/rec_applicants.php
@@ -0,0 +1,35 @@
+
+<?php include __DIR__ . '/../../layouts/sidebar_jobseeker.php'; ?>
+    <main class="content">
+      <div class="dashboard-main">
+        <div class="dashboard-container">
+          <div class="dashboard-header">
+            <h1 class="title">Applicants</h1>
+            <p class="subtitle">Review and Manage job Application</p>
+          </div>
+          <div class="dashboard-list-container">
+            <div class="dashboard-card-grid">
+              <div class="dashboard-card">
+                <div class="card-header">
+                  <div class="card-avatar">S</div>
+                  <div class="header-title-job">
+                    <h3 class="card-title">Mili</h3>
+                    <p class="card-subtitle">mili@bro.com</p>
+                  </div>
+                </div>
+                
+                <div class="card-info applicant-info">
+                  <p class="check">Applied For</p>
+                  <p class="check">Senior Sofware Engineer</p>
+                  <p class="check">Applied on Jan 15 2025</p>
+                </div>
+                <div class="card-actions">
+                  <a href="#" class="btn-primary action-btn">Resume</a>
+                  <a href="jobDetails.php" class="btn-secondary action-btn">Contact</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
