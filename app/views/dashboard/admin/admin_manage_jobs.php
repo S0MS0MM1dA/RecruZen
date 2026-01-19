742bfab857a05564c3c71c422c4677459eb210bb
zobayeralom <zobayermiraj10@gmail.com>
2026-01-19 19:26:10 +0600

    Manage Job

---
+++ app/views/dashboard/admin/admin_manage_jobs.php
@@ -0,0 +1,44 @@
+<?php include __DIR__ . '/../../layouts/sidebar_admin.php'; ?>
+   <main>
+     <div class="dashboard-main">
+       <div class="dashboard-container">
+         <div class="dashboard-header">
+           <h3>Job Approval</h3>
+           <p>Manage all the jobs!</p>
+         </div>
+
+         <section class="dashboard-tracker-div">
+           <div class="tracker-header">
+             <h4>JOb Information</h4>
+             <p>Track jobs</p>
+           </div>
+           <div class="tracker-table-div">
+             <table class="tracker-table">
+               <thead>
+                 <tr>
+                   <th>Job ID</th>
+                   <th>Posted By</th>
+                   <th>Title</th>
+                   <th>Description</th>
+                   <th>Category</th>
+                   <th>Posted on</th>
+                   <th>Action</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 <tr>
+                   <td>1</td>
+                   <td>Zobayer Alom</td>
+                   <td>SWE</td>
+                   <td>Description</td>
+                   <td>IT</td>
+                   <td>Jan 20, 2026</td>
+                   <td>Block</td>
+                 </tr>
+               </tbody>
+             </table>
+           </div>
+         </section>
+       </div>
+     </div>
+   </main>
